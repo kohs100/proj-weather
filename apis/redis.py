15aa4b49ea82b13105cdb968a19a977ef5a211b6
Ko Hyeongseok <kohs102@gmail.com>
2022-07-21 17:30:08 +0900

    pending

--- apis/redis.py
+++ apis/redis.py
@@ -1,7 +1,27 @@
 import aioredis
+import asyncio
+
+from secret import Secret
+from apis.weather import Forecast
+from apis.times import *
 
 class Cache():
     def __init__(self, host, port):
         self._db = aioredis.from_url(f"redis://{host}:{port}")
-    
-    # TODO: 쿼리 캐시 스키마 결정
+
+    async def get(self, nx, ny):
+        key = str(btime()) + "%03d%03d" % (nx, ny)
+        res = await self._db.setnx(key, "LOCKED")
+
+        if res:
+            # 치환 성공. 소유권 획득
+            fcst = Forecast(nx, ny)
+            await self._db.set(key, fcst.json())
+            return fcst.json()
+        else:
+            res = await self._db.get(key)
+            if res == 'LOCKED':
+                asyncio.sleep(1)
+                pass
+            else:
+                return res
